44ef1b59584b33872b34e03c323b5e0e0048280f
dangerye <dangerye99@gmail.com>
2019-10-01 02:57:52 +0800

    异步请求代码提交

---
+++ src/main/java/com/dangerye/powerful/communicate/AsyncLoad.java
@@ -0,0 +1,56 @@
+package com.dangerye.powerful.communicate;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 同步并行加载数据(减少‘多数据’整合业务的请求响应时间)
+ * 1、可以设置超时时间； 2、超时后正在执行的代码通过异常进行中断；
+ * 注意点：
+ * 由于超时是通过抛异常进行中断，所以 load() 方法中 try catch 代码需要特别注意，以免出错导致线程没被销毁；
+ * 相关博文：
+ * https://blog.csdn.net/li12412414/article/details/54577034
+ */
+public final class AsyncLoad<T> {
+
+    private ExecutorService executorService;
+    private FutureTask<T> futureTask;
+
+    private AsyncLoad() {
+        executorService = Executors.newSingleThreadExecutor();
+    }
+
+    public static <T> AsyncLoad<T> load(Callable<T> callable) {
+        AsyncLoad<T> asyncLoad = new AsyncLoad<>();
+        asyncLoad.futureTask = new FutureTask<>(callable);
+        asyncLoad.executorService.execute(asyncLoad.futureTask);
+        return asyncLoad;
+    }
+
+    public T get(long timeout) throws InterruptedException, ExecutionException, TimeoutException {
+        T result;
+        try {
+            result = futureTask.get(timeout, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            futureTask.cancel(true);
+            throw e;
+        } finally {
+            executorService.shutdown();
+        }
+        return result;
+    }
+
+    public T getIgnoreException(long timeout) {
+        try {
+            return get(timeout);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ src/test/java/com/dangerye/powerful/communicate/AsyncLoadTest.java
@@ -0,0 +1,53 @@
+package com.dangerye.powerful.communicate;
+
+import org.junit.Test;
+
+public class AsyncLoadTest {
+
+    @Test
+    public void testAsyncLoad() throws InterruptedException {
+        System.out.println("start");
+        long time = System.currentTimeMillis();
+        AsyncLoad<Integer> asyncLoad1 = AsyncLoad.load(() -> {
+            System.out.println("thread 1 start");
+            Thread.sleep(9000);
+            System.out.println("thread 1 end");
+            return 1;
+        });
+        AsyncLoad<Integer> asyncLoad2 = AsyncLoad.load(() -> {
+            System.out.println("thread 2 start");
+            Thread.sleep(7000);
+            System.out.println("thread 2 end");
+            return 2;
+        });
+        AsyncLoad<Integer> asyncLoad3 = AsyncLoad.load(() -> {
+            System.out.println("thread 3 start");
+            Thread.sleep(5000);
+            System.out.println("thread 3 end");
+            return 3;
+        });
+        AsyncLoad<Integer> asyncLoad4 = AsyncLoad.load(() -> {
+            System.out.println("thread 4 start");
+            Thread.sleep(3000);
+            System.out.println("thread 4 end");
+            return 4;
+        });
+        AsyncLoad<Integer> asyncLoad5 = AsyncLoad.load(() -> {
+            System.out.println("thread 5 start");
+//            Thread.sleep(1000);
+            throw new Exception("test");
+//            System.out.println("thread 5 end");
+//            return 5;
+        });
+
+        System.out.println(asyncLoad1.getIgnoreException(2000));
+        System.out.println(asyncLoad2.getIgnoreException(2000));
+        System.out.println(asyncLoad3.getIgnoreException(2000));
+        System.out.println(asyncLoad4.getIgnoreException(2000));
+        System.out.println(asyncLoad5.getIgnoreException(2000));
+
+        System.out.println("end. time : " + (System.currentTimeMillis() - time) + "ms");
+
+        Thread.sleep(10000);
+    }
+}
