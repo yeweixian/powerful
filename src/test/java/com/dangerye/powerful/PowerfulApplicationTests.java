ba53809191748510011e2a7da879e3bdcd867ef9
dangerye <dangerye99@gmail.com>
2019-10-03 16:49:07 +0800

    test code adjust

--- src/test/java/com/dangerye/powerful/PowerfulApplicationTests.java
+++ src/test/java/com/dangerye/powerful/PowerfulApplicationTests.java
@@ -4,7 +4,9 @@ import com.alibaba.fastjson.JSON;
 import com.dangerye.powerful.communicate.ThreadContext;
 import com.dangerye.powerful.service.TestService;
 import com.dangerye.powerful.utils.IpUtils;
+import com.dangerye.powerful.utils.LogUtils;
 import com.dangerye.powerful.utils.SpringContextUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Objects;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PowerfulApplicationTests {
@@ -38,6 +41,6 @@ public class PowerfulApplicationTests {
     public void testSpringContextUtils() {
         TestService service = SpringContextUtils.getBean(TestService.class);
         String test = service.getTestMsg("DangerYe");
-        System.out.println("testMsg: " + test);
+        LogUtils.info(log, "PRINT_EVENT", "testMsg:{}", test);
     }
 }
