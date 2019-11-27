126589e4ca876a9cc0a8793497c8c1276751537c
dangerye <dangerye99@gmail.com>
2019-11-27 21:36:28 +0800

    test code adjust

--- src/test/java/com/dangerye/powerful/HttpTest.java
+++ src/test/java/com/dangerye/powerful/HttpTest.java
@@ -1,13 +1,18 @@
 package com.dangerye.powerful;
 
 import com.dangerye.powerful.communicate.http.HttpInvoker;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
 import org.junit.Test;
 
 public class HttpTest {
 
     @Test
     public void testHttpDome1() {
-        String resp = HttpInvoker.execute(null, null, null);
+        HttpUriRequest request = RequestBuilder
+                .get("https://api.hearthstonejson.com/v1/latest/zhCN/cards.collectible.json")
+                .build();
+        String resp = HttpInvoker.execute(null, request, null);
         System.out.println("resp" + resp);
     }
 }
