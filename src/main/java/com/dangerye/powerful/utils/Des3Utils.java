2db741a8b6d1233f4174cc472184473e2d897e7c
dangerye <dangerye99@gmail.com>
2019-11-29 12:02:59 +0800

    base64 替换 被去掉的类

--- src/main/java/com/dangerye/powerful/utils/Des3Utils.java
+++ src/main/java/com/dangerye/powerful/utils/Des3Utils.java
@@ -1,19 +1,17 @@
 package com.dangerye.powerful.utils;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
 import javax.crypto.Cipher;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESedeKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Base64;
 
 public class Des3Utils {
 
     public static String encodeECB(String plainData, String key) throws Exception {
         byte[] dataBuffer = plainData.getBytes(StandardCharsets.UTF_8);
-        byte[] keyByte = new BASE64Decoder().decodeBuffer(key);
+        byte[] keyByte = Base64.getDecoder().decode(key);
 
         DESedeKeySpec spec = new DESedeKeySpec(keyByte);
         SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("desede");
@@ -24,12 +22,12 @@ public class Des3Utils {
 
         byte[] byteOut = cipher.doFinal(dataBuffer);
 
-        return new BASE64Encoder().encode(byteOut);
+        return Base64.getEncoder().encodeToString(byteOut);
     }
 
     public static String decodeECB(String dataToDecode, String key) throws Exception {
-        byte[] dataBuffers = new BASE64Decoder().decodeBuffer(dataToDecode);
-        byte[] keyBuffers = new BASE64Decoder().decodeBuffer(key);
+        byte[] dataBuffers = Base64.getDecoder().decode(dataToDecode);
+        byte[] keyBuffers = Base64.getDecoder().decode(key);
 
         DESedeKeySpec spec = new DESedeKeySpec(keyBuffers);
         SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("desede");
