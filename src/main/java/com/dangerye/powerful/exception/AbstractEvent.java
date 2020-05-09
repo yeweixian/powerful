244a22e184669cdc37708c1445234762371681bb
dangerye <dangerye99@gmail.com>
2020-05-09 15:55:12 +0800

    create event

---
+++ src/main/java/com/dangerye/powerful/exception/AbstractEvent.java
@@ -0,0 +1,42 @@
+package com.dangerye.powerful.exception;
+
+import org.apache.commons.lang3.StringUtils;
+
+public abstract class AbstractEvent implements Event {
+
+    private String business;
+    private String scene;
+    private int code;
+    private String message;
+
+    public AbstractEvent(String business, String scene, int code, String message) {
+        this.business = business;
+        this.scene = scene;
+        this.code = code;
+        this.message = message;
+    }
+
+    protected abstract int getDefaultCode();
+
+    protected abstract String getDefaultMessage();
+
+    @Override
+    public String getBusiness() {
+        return business;
+    }
+
+    @Override
+    public String getScene() {
+        return scene;
+    }
+
+    @Override
+    public int getCode() {
+        return code != 0 ? code : getDefaultCode();
+    }
+
+    @Override
+    public String getMessage() {
+        return StringUtils.isNotBlank(message) ? message : getDefaultMessage();
+    }
+}
