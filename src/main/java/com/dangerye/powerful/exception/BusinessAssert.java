df723e1080a5797ee42cc93863fb35768b0ccb32
dangerye <dangerye99@gmail.com>
2020-05-12 09:53:03 +0800

    adjust code

--- src/main/java/com/dangerye/powerful/exception/BusinessAssert.java
+++ src/main/java/com/dangerye/powerful/exception/BusinessAssert.java
@@ -6,24 +6,33 @@ import java.util.function.Function;
 
 public final class BusinessAssert {
 
-    public static void handleEvent(Event event, Function<Event, String> dynamicMessageFunc) {
+    public static BusinessException fail(Event event, Function<Event, String> dynamicMessageFunc) {
         Args.notNull(event, "event");
         String msg = dynamicMessageFunc.apply(event);
-        throw new BusinessException(Sign.builder()
+        return new BusinessException(Sign.builder()
                 .code(event.getCode())
                 .msg(msg)
                 .build());
     }
 
-    public static void handleEvent(Event event, Function<Event, String> dynamicMessageFunc,
-                                   String originalCode, String originalMsg) {
+    public static BusinessException fail(Event event, Function<Event, String> dynamicMessageFunc,
+                                         String originalCode, String originalMsg) {
         Args.notNull(event, "event");
         String msg = dynamicMessageFunc.apply(event);
-        throw new BusinessException(Sign.builder()
+        return new BusinessException(Sign.builder()
                 .code(event.getCode())
                 .msg(msg)
                 .originalCode(originalCode)
                 .originalMsg(originalMsg)
                 .build());
+    }
+
+    public static void handleEvent(Event event, Function<Event, String> dynamicMessageFunc) {
+        throw fail(event, dynamicMessageFunc);
+    }
+
+    public static void handleEvent(Event event, Function<Event, String> dynamicMessageFunc,
+                                   String originalCode, String originalMsg) {
+        throw fail(event, dynamicMessageFunc, originalCode, originalMsg);
     }
 }
