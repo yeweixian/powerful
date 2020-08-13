1b909de796f84b878fc68fbb3202e99efdf4dd65
dangerye <dangerye99@gmail.com>
2020-08-13 17:50:39 +0800

    update test code

--- src/test/java/com/dangerye/powerful/ProxyTest.java
+++ src/test/java/com/dangerye/powerful/ProxyTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.lang.reflect.Type;
 
 public class ProxyTest {
 
@@ -19,9 +18,10 @@ public class ProxyTest {
     @Test
     public void test2() {
         Intf intf = new Impl();
-        intf.doSomething("now", "InTest2");
-        Intf proxy = (Intf) Proxy.newProxyInstance(Handler.class.getClassLoader(), intf.getClass().getInterfaces(), new Handler(intf));
-        proxy.doSomething("now", "InProxy");
+//        intf.doSomething("now", "InTest2");
+        intf = (Intf) Proxy.newProxyInstance(Handler.class.getClassLoader(), intf.getClass().getInterfaces(), new Handler(intf));
+        intf = (Intf) Proxy.newProxyInstance(Handler.class.getClassLoader(), intf.getClass().getInterfaces(), new Handler(intf));
+        intf.doSomething("now", "InProxy");
         Class<?>[] classes1 = Intf.class.getClasses();
         Class<?>[] classes2 = Intf.class.getDeclaredClasses();
         Class<?>[] classes3 = Intf.class.getInterfaces();
@@ -53,9 +53,9 @@ public class ProxyTest {
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            Type genericReturnType = method.getGenericReturnType();
-            System.out.println(genericReturnType.equals(Void.TYPE));
-            System.out.println("genericReturnType: " + genericReturnType.getTypeName());
+//            Type genericReturnType = method.getGenericReturnType();
+//            System.out.println(genericReturnType.equals(Void.TYPE));
+//            System.out.println("genericReturnType: " + genericReturnType.getTypeName());
             System.out.println("------");
             System.out.println("method: " + method.getName());
             System.out.println("classname: " + method.getDeclaringClass().getSimpleName());
@@ -65,6 +65,12 @@ public class ProxyTest {
             try {
                 return method.invoke(proxyClass, args);
             } catch (Throwable t) {
+                Throwable cause = t.getCause();
+                if (cause instanceof NullPointerException) {
+                    System.out.println("NullPointerException");
+                } else {
+                    System.out.println("OtherException");
+                }
                 if (t instanceof NullPointerException) {
                     System.out.println("NullPointerException");
                 } else {
