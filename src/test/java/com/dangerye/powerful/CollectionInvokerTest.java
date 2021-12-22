33e7323efbf59dfce921c177822bb64654a715c9
dangerye <dangerye@me.com>
2021-12-22 14:29:15 +0800

    淘汰不用的工具，使用生产在用的工具类

--- src/test/java/com/dangerye/powerful/CollectionInvokerTest.java
+++ src/test/java/com/dangerye/powerful/CollectionInvokerTest.java
@@ -2,7 +2,6 @@ package com.dangerye.powerful;
 
 import com.alibaba.fastjson.JSON;
 import com.dangerye.powerful.collection.CollectionInvoker;
-import com.dangerye.powerful.collection.NewCollectionInvoker;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import lombok.Builder;
@@ -11,47 +10,11 @@ import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CollectionInvokerTest {
-
-    @Test
-    public void testNewInvoker() {
-        final ArrayList<Integer> integerList = Lists.newArrayList();
-        for (int i = 0; i < 10; i++) {
-            integerList.add(RandomUtils.nextInt(0, 10));
-        }
-        final List<Item> list = CollectionInvoker.changeList(integerList,
-                item -> Item.builder().value(item).build());
-        System.out.println("before invoke: " + JSON.toJSONString(list));
-        final NewCollectionInvoker.AbstractFilter<Item, Map<String, Object>> filter
-                = new NewCollectionInvoker.AbstractFilter<Item, Map<String, Object>>() {
-            @Override
-            protected boolean doFilter(Item item, Map<String, Object> context) {
-                return item.getValue() % 3 != 0;
-            }
-        };
-        final NewCollectionInvoker.InvokerInterceptor<Item, Map<String, Object>> interceptor
-                = invocation -> {
-            final int beforeSum = invocation.getCollection().stream().mapToInt(Item::getValue).sum();
-            invocation.getContext().put("beforeSum", beforeSum);
-            invocation.proceed();
-            final int afterSum = invocation.getCollection().stream().mapToInt(Item::getValue).sum();
-            invocation.getContext().put("afterSum", afterSum);
-        };
-        final HashMap<String, Object> context = Maps.newHashMap();
-        final NewCollectionInvoker<Item, Map<String, Object>> invoker = NewCollectionInvoker.<Item, Map<String, Object>>builder()
-                .context(context)
-                .interceptors(Lists.newArrayList(interceptor))
-                .filters(Lists.newArrayList(filter))
-                .build();
-        invoker.invoke(list);
-        System.out.println("after invoke: " + JSON.toJSONString(list));
-        System.out.println("context: " + JSON.toJSONString(context));
-    }
 
     @Test
     public void testInvoker() {
@@ -62,32 +25,26 @@ public class CollectionInvokerTest {
         final List<Item> list = CollectionInvoker.changeList(integerList,
                 item -> Item.builder().value(item).build());
         System.out.println("before invoke: " + JSON.toJSONString(list));
-        final HashMap<String, Object> context = Maps.newHashMap();
-        CollectionInvoker.AbstractFilter<Item, Map<String, Object>> predicate
+        final CollectionInvoker.AbstractFilter<Item, Map<String, Object>> filter
                 = new CollectionInvoker.AbstractFilter<Item, Map<String, Object>>() {
             @Override
             protected boolean doFilter(Item item, Map<String, Object> context) {
                 return item.getValue() % 3 != 0;
             }
         };
-        CollectionInvoker.InvokerInterceptor<Item, Map<String, Object>> interceptor
-                = new CollectionInvoker.InvokerInterceptor<Item, Map<String, Object>>() {
-            @Override
-            public void beforeInvoke(Collection<Item> collection, Map<String, Object> context) {
-                final int beforeSum = collection.stream().mapToInt(Item::getValue).sum();
-                context.put("beforeSum", beforeSum);
-            }
-
-            @Override
-            public void afterInvoke(Collection<Item> collection, Map<String, Object> context) {
-                final int afterSum = collection.stream().mapToInt(Item::getValue).sum();
-                context.put("afterSum", afterSum);
-            }
+        final CollectionInvoker.InvokerInterceptor<Item, Map<String, Object>> interceptor
+                = invocation -> {
+            final int beforeSum = invocation.getCollection().stream().mapToInt(Item::getValue).sum();
+            invocation.getContext().put("beforeSum", beforeSum);
+            invocation.proceed();
+            final int afterSum = invocation.getCollection().stream().mapToInt(Item::getValue).sum();
+            invocation.getContext().put("afterSum", afterSum);
         };
-        CollectionInvoker<Item, Map<String, Object>> invoker = CollectionInvoker.<Item, Map<String, Object>>builder()
+        final HashMap<String, Object> context = Maps.newHashMap();
+        final CollectionInvoker<Item, Map<String, Object>> invoker = CollectionInvoker.<Item, Map<String, Object>>builder()
                 .context(context)
                 .interceptors(Lists.newArrayList(interceptor))
-                .filters(Lists.newArrayList(predicate))
+                .filters(Lists.newArrayList(filter))
                 .build();
         invoker.invoke(list);
         System.out.println("after invoke: " + JSON.toJSONString(list));
