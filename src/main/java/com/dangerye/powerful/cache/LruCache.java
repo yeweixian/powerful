8a10b5d0774040ff456fa33cdd28c8824ca3c4dd
dangerye <dangerye@me.com>
2021-12-26 21:52:58 +0800

    init lru cache

--- src/main/java/com/dangerye/powerful/cache/LruCache.java
+++ src/main/java/com/dangerye/powerful/cache/LruCache.java
@@ -1,9 +1,11 @@
 package com.dangerye.powerful.cache;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 public final class LruCache<K, V> {
 
+    private final ReentrantLock lock;
     private final ConcurrentHashMap<K, Node<K, V>> cache;
     private final Node<K, V> begin;
     private final Node<K, V> end;
@@ -13,7 +15,8 @@ public final class LruCache<K, V> {
     private LruCache(int hotSize, long survivalTime) {
         this.hotSize = hotSize;
         this.survivalTime = survivalTime;
-        this.cache = new ConcurrentHashMap<>(hotSize);
+        this.lock = new ReentrantLock();
+        this.cache = new ConcurrentHashMap<>(hotSize + 1);
         this.begin = new Node<>();
         this.end = new Node<>();
     }
@@ -22,11 +25,25 @@ public final class LruCache<K, V> {
         return new LruCache<>(hotSize, survivalTime);
     }
 
+    public void put(K key, V value) {
+        long expireTime = System.currentTimeMillis() + survivalTime;
+
+    }
+
     private static class Node<K, V> {
         private K key;
         private V value;
+        private long expireTime;
         private Node<K, V> prev;
         private Node<K, V> next;
-        private long expireTime;
+
+        public Node() {
+        }
+
+        public Node(K key, V value, long expireTime) {
+            this.key = key;
+            this.value = value;
+            this.expireTime = expireTime;
+        }
     }
 }
