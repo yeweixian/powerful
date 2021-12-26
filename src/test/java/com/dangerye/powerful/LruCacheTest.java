643aa0cff59e64de1c6750b4e146cc7a373420df
dangerye <dangerye@me.com>
2021-12-27 01:38:39 +0800

    com.dangerye.powerful.LruCacheTest.testCache done

--- src/test/java/com/dangerye/powerful/LruCacheTest.java
+++ src/test/java/com/dangerye/powerful/LruCacheTest.java
@@ -1,17 +1,21 @@
 package com.dangerye.powerful;
 
 import com.dangerye.powerful.cache.LruCache;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 
 public class LruCacheTest {
 
     @Test
     public void testCache() {
-        final LruCache<Integer, Integer> cache = LruCache.initCache(0, 1000L * 10);
+        final LruCache<Integer, Integer> cache = LruCache.initCache(2, 1000L * 10);
         for (int i = 0; i < 10; i++) {
-            cache.put(i, i);
+            final int key = RandomUtils.nextInt(0, 4);
+            final int value = RandomUtils.nextInt(100, 1000);
+            System.out.printf("key:%s, value:%s%n", key, value);
+            cache.put(key, value);
         }
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 4; i++) {
             System.out.println(cache.get(i));
         }
     }
