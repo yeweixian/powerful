4427be069a6f6f51c0e3b19aaf5eb034a7fdebf8
dangerye <dangerye@me.com>
2022-07-22 14:23:27 +0800

    Update AbstractInvoker code

--- src/main/java/com/dangerye/powerful/communicate/AbstractInvoker.java
+++ src/main/java/com/dangerye/powerful/communicate/AbstractInvoker.java
@@ -1,98 +1,67 @@
 package com.dangerye.powerful.communicate;
 
-import java.util.Collection;
-import java.util.Optional;
-import java.util.concurrent.Callable;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.function.Consumer;
 import java.util.function.Function;
 
-public abstract class AbstractInvoker<C extends Invoker.Context, E extends Throwable> implements Invoker {
+@Slf4j
+public abstract class AbstractInvoker<T, C extends InvokeContext<? extends T>, E extends Throwable> extends Invoker<T, C> {
 
-    private final GetFunction<C, E> getFunction;
-    private final ThrowFunction<C, E> throwFunction;
-
-    protected AbstractInvoker() {
-        this.getFunction = new GetFunction<C, E>() {
-            @Override
-            @SuppressWarnings("unchecked")
-            public <R> R handle(C context, Consumer<E> consumer) {
-                try {
-                    return (R) getProxy(() -> coreCode(context), context).call();
-                } catch (Exception e) {
-                    if (consumer != null) {
-                        final E exception = transformException(context, e);
-                        consumer.accept(exception);
-                    }
-                    return null;
-                }
-            }
-        };
-        this.throwFunction = new ThrowFunction<C, E>() {
-            @Override
-            @SuppressWarnings("unchecked")
-            public <R, T extends Throwable> R handle(C context, Function<E, ? extends T> function) throws T {
-                try {
-                    return (R) getProxy(() -> coreCode(context), context).call();
-                } catch (Exception e) {
-                    final E exception = transformException(context, e);
-                    throw function.apply(exception);
-                }
-            }
-        };
-    }
-
-    private <R> Callable<R> getProxy(final Callable<R> callable, final C context) {
-        Callable<R> plugin = callable;
-        final Collection<Interceptor> interceptors = logicInterceptors(context);
-        if (interceptors != null) {
-            for (Interceptor interceptor : interceptors) {
-                if (interceptor != null) {
-                    plugin = interceptor.plugin(plugin, context);
-                }
-            }
+    private void writeLog(C context, Exception exception) {
+        if (log.isWarnEnabled()) {
+            final String invokeEvent = context.getInvokeEvent();
+            log.warn("[Invoker.Fail] msg = invokeEvent:{} invoker fail. ", invokeEvent, exception);
         }
-        return plugin;
     }
 
-    protected abstract <R> R coreCode(final C context) throws Exception;
+    public final <R> R get(final C context) {
+        try {
+            return super.execute(context);
+        } catch (Exception e) {
+            writeLog(context, e);
+            return null;
+        }
+    }
 
-    protected abstract Collection<Interceptor> logicInterceptors(final C context);
+    public final <R> R get(final C context, final Consumer<E> consumer) {
+        try {
+            return super.execute(context);
+        } catch (Exception e) {
+            writeLog(context, e);
+            if (consumer != null) {
+                final E exception = transformException(context, e);
+                consumer.accept(exception);
+            }
+            return null;
+        }
+    }
+
+    public final <R, TE extends Throwable> R getOrThrow(final C context, final Function<E, ? extends TE> function) throws TE {
+        try {
+            return super.execute(context);
+        } catch (Exception e) {
+            writeLog(context, e);
+            final E exception = transformException(context, e);
+            throw function.apply(exception);
+        }
+    }
+
+    public final <R, TE extends Throwable> R getElseThrow(final C context, final Function<E, ? extends TE> function) throws TE {
+        R result = null;
+        try {
+            result = super.execute(context);
+        } catch (Exception e) {
+            writeLog(context, e);
+            final E exception = transformException(context, e);
+            throw function.apply(exception);
+        }
+        if (result != null) {
+            return result;
+        } else {
+            throw function.apply(null);
+        }
+    }
 
     protected abstract E transformException(final C context, final Exception exception);
-
-    @SuppressWarnings("unchecked")
-    public final <R> R get(final C context) {
-        return (R) getFunction.handle(context, null);
-    }
-
-    @SuppressWarnings("unchecked")
-    public final <R> R get(final C context, final Consumer<E> consumer) {
-        return (R) getFunction.handle(context, consumer);
-    }
-
-    @SuppressWarnings("unchecked")
-    public final <R, T extends Throwable> R getOrThrow(final C context, final Function<E, ? extends T> function) throws T {
-        return (R) throwFunction.handle(context, function);
-    }
-
-    @SuppressWarnings("unchecked")
-    public final <R, T extends Throwable> R getElseThrow(final C context, final Function<E, ? extends T> function) throws T {
-        final ExceptionBridging<E> bridging = new ExceptionBridging<>();
-        return Optional.ofNullable((R) getFunction.handle(context, exception -> bridging.exception = exception))
-                .orElseThrow(() -> function.apply(bridging.exception));
-    }
-
-    @FunctionalInterface
-    private interface GetFunction<C extends Context, E extends Throwable> {
-        <R> R handle(final C context, final Consumer<E> consumer);
-    }
-
-    @FunctionalInterface
-    private interface ThrowFunction<C extends Context, E extends Throwable> {
-        <R, T extends Throwable> R handle(final C context, final Function<E, ? extends T> function) throws T;
-    }
-
-    private static final class ExceptionBridging<E> {
-        private E exception;
-    }
 }
