2253e0e308c601d69c81cfb0d7f3c5ce93ac9b16
dangerye <dangerye@me.com>
2022-07-29 14:49:35 +0800

    code adjust

--- src/main/java/com/dangerye/powerful/communicate/AbstractCollectionInvoker.java
+++ src/main/java/com/dangerye/powerful/communicate/AbstractCollectionInvoker.java
@@ -9,20 +9,20 @@ import org.springframework.util.Assert;
 import java.util.Collection;
 
 @Slf4j
-public abstract class AbstractCollectionInvoker<I, C extends InvokeContext<? extends Collection<? extends I>>> extends Invoker<C> {
+public abstract class AbstractCollectionInvoker<I, C extends Invoker.CollectionContext<I>> extends Invoker<C> {
 
     protected abstract Collection<CollectionFilter<? super I, ? super C>> invokeCollectionFilters(final C context);
 
     private void writeLog(C context, Exception exception) {
-        if (log.isWarnEnabled()) {
+        if (log.isDebugEnabled()) {
             final String invokeEvent = context.getInvokeEvent();
-            log.warn("[CollectionInvoker.Fail] msg = invokeEvent:{} invoker fail. ", invokeEvent, exception);
+            log.debug("[CollectionInvoker.Fail] msg = invokeEvent:{} invoker fail. ", invokeEvent, exception);
         }
     }
 
     @Override
     protected <R> R coreCode(C context) throws Exception {
-        final Collection<? extends I> target = context.getTarget();
+        final Collection<? extends I> target = context.getCollection();
         final Collection<CollectionFilter<? super I, ? super C>> collectionFilters = invokeCollectionFilters(context);
         try (CloseableContext<C> closeableContext = new CloseableContext<>(getConfigures(collectionFilters))) {
             closeableContext.configure(context);
@@ -34,7 +34,7 @@ public abstract class AbstractCollectionInvoker<I, C extends InvokeContext<? ext
 
     public final void invoke(final C context) {
         Assert.notNull(context, "context must not be null");
-        Assert.notNull(context.getTarget(), "target must not be null");
+        Assert.notNull(context.getCollection(), "collection must not be null");
         Assert.notNull(context.getInvokeEvent(), "invokeEvent must not be null");
         try {
             super.execute(context);
