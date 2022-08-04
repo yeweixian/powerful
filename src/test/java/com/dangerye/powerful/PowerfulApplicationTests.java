25293978e76ae54f573806ece52212991720eaff
dangerye <dangerye@me.com>
2022-08-04 17:58:16 +0800

    test Autowired Map

--- src/test/java/com/dangerye/powerful/PowerfulApplicationTests.java
+++ src/test/java/com/dangerye/powerful/PowerfulApplicationTests.java
@@ -1,6 +1,7 @@
 package com.dangerye.powerful;
 
 import com.alibaba.fastjson.JSON;
+import com.dangerye.powerful.communicate.Invoker;
 import com.dangerye.powerful.communicate.ThreadContext;
 import com.dangerye.powerful.service.TestService;
 import com.dangerye.powerful.utils.IpUtils;
@@ -11,11 +12,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -23,6 +27,9 @@ import java.util.Optional;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PowerfulApplicationTests {
+
+    @Autowired
+    private final Map<String, Invoker<Invoker.InvokeContext>> invokerMap = new HashMap<>();
 
     @Before
     public void before() {
@@ -38,6 +45,16 @@ public class PowerfulApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void testAutowiredMap() {
+        for (Map.Entry<String, Invoker<Invoker.InvokeContext>> entry : invokerMap.entrySet()) {
+            final String entryKey = entry.getKey();
+            final Invoker<Invoker.InvokeContext> entryValue = entry.getValue();
+            System.out.println("entryKey: " + entryKey);
+            System.out.println("entryValue: " + entryValue);
+        }
     }
 
     @Test
