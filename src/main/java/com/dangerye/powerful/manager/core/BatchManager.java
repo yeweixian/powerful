cec839d1962e62aa507deeab9f0475613acbb6ab
dangerye <dangerye@me.com>
2022-09-17 17:13:37 +0800

    BatchManager

--- src/main/java/com/dangerye/powerful/manager/core/BatchManager.java
+++ src/main/java/com/dangerye/powerful/manager/core/BatchManager.java
@@ -3,8 +3,16 @@ package com.dangerye.powerful.manager.core;
 import java.util.Map;
 
 public abstract class BatchManager {
+
+    private final UniversalContext universalContext;
+
     public BatchManager(UniversalContext universalContext) {
         universalContext.putIfAbsent(getBusinessEvent(), this);
+        this.universalContext = universalContext;
+    }
+
+    protected final UniversalContext getUniversalContext() {
+        return universalContext;
     }
 
     protected abstract String getBusinessEvent();
