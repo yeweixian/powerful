48c771dddf04013fb385f7df4d4d0b5ea3b1fab4
dangerye <dangerye@me.com>
2022-09-17 21:24:01 +0800

    test code

---
+++ src/test/java/com/dangerye/powerful/manager/batch/TestBatchManager1.java
@@ -0,0 +1,34 @@
+package com.dangerye.powerful.manager.batch;
+
+import com.dangerye.powerful.manager.core.BatchManager;
+import com.dangerye.powerful.manager.core.UniversalContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class TestBatchManager1 extends BatchManager {
+
+    @Autowired
+    public TestBatchManager1(UniversalContext universalContext) {
+        super(universalContext);
+    }
+
+    @Override
+    protected String getBusinessEvent() {
+        return "testBatch1";
+    }
+
+    @Override
+    protected Object handleBusiness(Map<String, String> paramMap) {
+        final String param1 = paramMap.get("testBatch1.param1");
+        final Batch1Data batch1Data = new Batch1Data();
+        batch1Data.msg = param1;
+        return batch1Data;
+    }
+
+    public static class Batch1Data {
+        private String msg;
+    }
+}
---
+++ src/test/java/com/dangerye/powerful/manager/batch/TestBatchManager2.java
@@ -0,0 +1,34 @@
+package com.dangerye.powerful.manager.batch;
+
+import com.dangerye.powerful.manager.core.BatchManager;
+import com.dangerye.powerful.manager.core.UniversalContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class TestBatchManager2 extends BatchManager {
+
+    @Autowired
+    public TestBatchManager2(UniversalContext universalContext) {
+        super(universalContext);
+    }
+
+    @Override
+    protected String getBusinessEvent() {
+        return "testBatch2";
+    }
+
+    @Override
+    protected Object handleBusiness(Map<String, String> paramMap) {
+        final String param2 = paramMap.get("testBatch2.param2");
+        final Batch2Data batch2Data = new Batch2Data();
+        batch2Data.msg = param2;
+        return batch2Data;
+    }
+
+    public static class Batch2Data {
+        private String msg;
+    }
+}
