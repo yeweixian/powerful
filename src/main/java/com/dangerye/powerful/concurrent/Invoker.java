2f030f2f41122d35e3e0f84447669d86d32d4933
dangerye <dangerye@me.com>
2024-04-13 00:51:33 +0800

    重构 v.2

--- src/main/java/com/dangerye/powerful/concurrent/Invoker.java
+++ src/main/java/com/dangerye/powerful/concurrent/Invoker.java
@@ -1,6 +1,7 @@
 package com.dangerye.powerful.concurrent;
 
 import java.util.Collection;
+import java.util.concurrent.Callable;
 
 public abstract class Invoker<C> {
 
@@ -11,10 +12,33 @@ public abstract class Invoker<C> {
             @Override
             public <R> R execute(C context) throws Exception {
                 final Collection<InvokeInterceptor<? super C>> interceptors = invokeInterceptors(context);
-
+                try (final InvokeSession<C> session = InvokeSession.init(interceptors)) {
+                    session.configure(context);
+                    final Callable<R> core = () -> coreCode(context);
+                    final Callable<R> proxy = getProxy(core, interceptors, context);
+                    return proxy.call();
+                }
             }
         };
     }
+
+    private static <R, S> Callable<R> getProxy(final Callable<R> callable, final Collection<InvokeInterceptor<? super S>> interceptors, S context) {
+        Callable<R> plugin = callable;
+        if (interceptors != null) {
+            for (InvokeInterceptor<? super S> interceptor : interceptors) {
+                if (interceptor != null) {
+                    plugin = interceptor.plugin(plugin, context);
+                }
+            }
+        }
+        return plugin;
+    }
+
+    protected final <R> R execute(C context) throws Exception {
+        return codeFunc.execute(context);
+    }
+
+    protected abstract <R> R coreCode(final C context) throws Exception;
 
     protected abstract Collection<InvokeInterceptor<? super C>> invokeInterceptors(final C context);
 
