436a2b22b5a20e02f011c57ece8415a77be30ef4
dangerye <dangerye@me.com>
2024-07-29 00:52:47 +0800

    尝试单例池 v3

--- src/main/java/com/dangerye/powerful/trial/Loader.java
+++ src/main/java/com/dangerye/powerful/trial/Loader.java
@@ -1,13 +1,45 @@
 package com.dangerye.powerful.trial;
 
-public final class Loader<T> {
-    private volatile T obj;
+import java.lang.reflect.Method;
 
-    public void set(T obj) {
-        this.obj = obj;
+public final class Loader<C> {
+    private final Class<C> clazz;
+    private volatile C instance;
+
+    public Loader(Class<C> clazz) {
+        this.clazz = clazz;
     }
 
-    public T get() {
-        return obj;
+    public void set(C instance) {
+        this.instance = instance;
+    }
+
+    public C get() {
+        return instance;
+    }
+
+    public C getInstance() {
+        if (instance == null) {
+            synchronized (this) {
+                if (instance == null) {
+                    return instance = createInstance();
+                } else {
+                    return instance;
+                }
+            }
+        } else {
+            return instance;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private C createInstance() {
+        try {
+            final Method staticMethod = clazz.getDeclaredMethod("getInstance");
+            staticMethod.setAccessible(true);
+            return (C) staticMethod.invoke(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
